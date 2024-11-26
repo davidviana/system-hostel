d3c71913eace717a98c714774212a73253b91c3d
Isabela Freitas Franco <francofreitasisabela@gmail.com>
2024-11-26 16:42:13 -0300

    starting config project + fix localhost port

--- src/page/LoginPage/login.js
+++ src/page/LoginPage/login.js
@@ -177,35 +177,6 @@ function LoginPage() {
                 </div>
             )}
 
-            <section className="login-gallery" id='galery-section'>
-                <h2>Galeria de Fotos</h2>
-                <div className="login-gallery-images">
-                    <img src={Item_1} alt="Imagem 1" />
-                    <img src={Item_2} alt="Imagem 2" />
-                    <img src={Item_3} alt="Imagem 3" />
-                </div>
-            </section>
-
-            <section className="login-banner">
-                <h2>Uma super experiência para contar!</h2>
-                <button>Reservar Agora</button>
-            </section>
-
-            <section className="login-accommodations" id='accomodations-section'>
-                <h2 id='acommodation-section'>Nossas Acomodações</h2>
-                <p>Quartos Individuais ou Compartilhados</p>
-                <div className="login-room">
-                    <img src={CoupleRoom} alt="Quarto Individual" />
-                    <h3>Quarto Individual</h3>
-                    <p><span>R$150</span> / noite</p>
-                </div>
-                <div className="login-room">
-                    <img src={SharedRoom} alt="Quarto Compartilhado" />
-                    <h3>Quarto Compartilhado</h3>
-                    <p><span>R$300</span> / noite</p>
-                </div>
-            </section>
-
             <Footer />
         </div>
     );
