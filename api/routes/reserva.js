f794a52f9a6a73125741f4179d165a4641e5fb7c
Isabela Freitas Franco <francofreitasisabela@gmail.com>
2024-12-05 23:25:16 -0300

    fixing reserva.js

--- api/routes/reserva.js
+++ api/routes/reserva.js
@@ -27,22 +27,16 @@ router.get('/:id', async (req, res) => {
 
 // POST: Adicionar uma nova reserva
 router.post('/', async (req, res) => {
-    const { date_reserva, data_checkin, data_checkout, cliente_id, room_number } = req.body;
+    const { date_reserva, data_checkin, data_checkout, cliente_id, room_number, dias_de_estadia, total_value } = req.body;
 
     try {
-        const resultQuarto = await pool.query('SELECT id FROM quarto WHERE numero = $1', [room_number]);
-        if (resultQuarto.rows.length === 0) {
-            return res.status(404).send('Quarto não encontrado');
-        }
-
         // Atualiza o status do quarto para 'reservado'
         await pool.query('UPDATE quarto SET status_quarto = $1 WHERE numero = $2', ["reservado", room_number]);
-        const quarto_id = resultQuarto.rows[0].id;
 
         // Cria a reserva
         const resultReserva = await pool.query(
-            'INSERT INTO reserva (data_reserva, data_checkin, data_checkout, status, cliente_id, quarto_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-            [date_reserva, data_checkin, data_checkout, 'ativa', cliente_id, quarto_id]
+            'INSERT INTO reserva (data_reserva, data_checkin, data_checkout, status, cliente_id, quarto_id, dias_de_estadia, is_checkin, is_checkout, total_value) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
+            [date_reserva, data_checkin, data_checkout, 'ativa', cliente_id, room_number, dias_de_estadia, 'false', 'false', total_value]
         );
 
         // Envia uma atualização via WebSocket para todos os clientes conectados
@@ -66,7 +60,7 @@ router.put('/cancelar', async (req, res) => {
     const { reserva_id, room_number } = req.body;
 
     try {
-        await pool.query('UPDATE quarto SET status_quarto = $1 WHERE id = $2', ['disponível', room_number]);
+        await pool.query('UPDATE quarto SET status_quarto = $1 WHERE numero = $2', ['disponível', room_number]);
         const resultReserva = await pool.query('UPDATE reserva SET status = $1 WHERE id = $2', ['cancelada', reserva_id]);
 
         broadcastUpdate({
@@ -81,13 +75,33 @@ router.put('/cancelar', async (req, res) => {
     }
 });
 
-// PUT: Confirmar uma reserva
+// PUT: Confirmar uma reserva + checkin
 router.put('/confirmar', async (req, res) => {
     const { reserva_id, room_number } = req.body;
 
     try {
-        await pool.query('UPDATE quarto SET status_quarto = $1 WHERE id = $2', ['ocupado', room_number]);
-        const resultReserva = await pool.query('UPDATE reserva SET status = $1 WHERE id = $2', ['conclu¡da', reserva_id]);
+        await pool.query('UPDATE quarto SET status_quarto = $1 WHERE numero = $2', ['ocupado', room_number]);
+        const resultReserva = await pool.query('UPDATE reserva SET status = $1, is_checkin = $2, is_checkout = $3 WHERE id = $4', ['conclu¡da', 'true', 'false',reserva_id]);
+
+        broadcastUpdate({
+            type: 'reservationUpdate',
+            status: 'confirmed',
+            roomId: room_number
+        });
+
+        res.status(201).json(resultReserva.rows[0]);
+    } catch (err) {
+        res.status(400).send('Erro ao confirmar reserva');
+    }
+});
+
+// PUT: Confirmar o checkout da reserva
+router.put('/checkout', async (req, res) => {
+    const { reserva_id, room_number } = req.body;
+
+    try {
+        await pool.query('UPDATE quarto SET status_quarto = $1 WHERE numero = $2', ['disponível',room_number]);
+        const resultReserva = await pool.query('UPDATE reserva SET status = $1, is_checkout = $2 WHERE id = $3', ['conclu¡da','true', reserva_id]);
 
         broadcastUpdate({
             type: 'reservationUpdate',
